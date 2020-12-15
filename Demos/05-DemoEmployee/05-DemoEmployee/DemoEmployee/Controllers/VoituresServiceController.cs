650477da10572ce8849943dfabc1ffd26d4f3bd5
gabb134 <cg.marrero@cgodin.qc.ca>
2020-12-14 19:26:46 -0500

    modif gab, cours termine

---
+++ Demos/05-DemoEmployee/05-DemoEmployee/DemoEmployee/Controllers/VoituresServiceController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoEmployee.Controllers
+{
+    public class VoituresServiceController : ApiController
+    {
+        // GET: api/VoituresService
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/VoituresService/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/VoituresService
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/VoituresService/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/VoituresService/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
